f14c60d30dd167577d31c57fccba44a0a9a54142
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-02 19:36:01 -0700

    Add a simple_upload function

--- R/upload.r
+++ R/upload.r
@@ -1,8 +1,20 @@
 base_url <- "https://www.googleapis.com/storage/v1/"
 upload_url <- "https://www.googleapis.com/upload/storage/v1/"
 
+#' A simple upload of a file to a Cloud Storage bucket
+#' @param file Name of the uploaded file.
+#' @param bucket Bucket name.
+#' @param name Object name.
+#' @param type Media type.
+#' @param token A valid OAuth2.0 token.
+#' @export
 simple_upload <- function(file, bucket, name = "uploadedObject", type = NULL,
                           token = get_access_cred()) {
+  assert_that(is.string(file),
+              is.string(bucket),
+              is.string(name),
+              is.null(type) || is.string(type))
+
   url <- paste0(upload_url, "b/", bucket, "/o")
   
   if (is.null(type)) {
@@ -13,4 +25,6 @@ simple_upload <- function(file, bucket, name = "uploadedObject", type = NULL,
                config(token = token),
                add_headers("Content-Type" = type),
                query = list(uploadType = "media", name = name))
+  
+  resp
 }
