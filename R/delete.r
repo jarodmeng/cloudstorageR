443153c6d67fc3177be470ad57395d0a70d7f7f5
Jarod G.R. Meng <genrong.meng@gmail.com>
2016-08-06 23:01:23 -0700

    Add documentation for delete_obj()

--- R/delete.r
+++ R/delete.r
@@ -1,5 +1,10 @@
 base_url <- "https://www.googleapis.com/storage/v1/"
 
+#' Delete an object in a bucket.
+#' @param bucket bucket name
+#' @param object object name
+#' @param token a valid OAuth2.0 token
+#' @return TRUE if the object is successfully deleted; FALSE otherwise
 #' @importFrom httr DELETE
 #' @export
 delete_obj <- function(bucket, object, token = get_access_cred()) {
